efe50516e9a0037341d3af6fba2fe76f75259d8c
KostiantynU <115901013+KostiantynU@users.noreply.github.com>
2023-04-14 23:54:20 +0300

    most work

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,12 @@
+const BASIC_URL = 'https://restcountries.com/v3.1/name/';
+
 export function fetchCountries(country, { official, capital, population, flags, languages }) {
   return fetch(
-    `https://restcountries.com/v3.1/name/${country}?fields=${official},${capital},${population},${flags},${languages}`
+    `${BASIC_URL}${country}?fields=${official},${capital},${population},${flags},${languages}`
   )
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => data)
+    .catch(error => {
+      console.log(error);
+    });
 }
-
-// https://restcountries.com/v3.1/{service}?fields={field},{field},{field
-// name.official,capital,population,flag.svg,languages
-// ?fields=name.official,capital,population,flags.svg,languages
-// ?fulltext=true&fields=name,capital,population,flags,languages
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
@@ -9,19 +10,78 @@ const DEBOUNCE_DELAY = 300;
 const refs = {
   searchBoxEl: document.querySelector('#search-box'),
   countryListEl: document.querySelector('.country-list'),
-  countryInfoEl: document.querySelector('.countryInfo'),
-};
+  countryInfoEl: document.querySelector('.country-info'),
 
-const optionsObj = {
-  official: 'name',
-  capital: 'capital',
-  population: 'population',
-  flags: 'flags',
-  languages: 'languages',
+  optionsObj: {
+    official: 'name',
+    capital: 'capital',
+    population: 'population',
+    flags: 'flags',
+    languages: 'languages',
+  },
+
+  notiflixOpt: { timeout: 1000 },
 };
 
 refs.searchBoxEl.addEventListener('input', debounce(searchByWord, DEBOUNCE_DELAY));
 
 function searchByWord(event) {
-  fetchCountries(event.target.value, optionsObj);
+  if (!event.target.value.trim()) {
+    Notiflix.Notify.info('The field is empty.', refs.notiflixOpt);
+    refs.countryInfoEl.innerHTML = '';
+    return;
+  }
+  const countriesPromise = fetchCountries(event.target.value.trim(), refs.optionsObj);
+  countriesPromise
+    .then(array => {
+      if (array.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.',
+          refs.notiflixOpt
+        );
+        refs.searchBoxEl.value = '';
+        console.log(refs.searchBoxEl);
+      } else if (array.length < 10 && array.length > 2) {
+        renderList(createMarkupList(array));
+      } else if (array.length === 1) {
+        renderCard(createMarkupCard(array));
+      }
+    })
+    .catch(onError);
+}
+
+function createMarkupList(array) {
+  return (listCountries = array
+    .map(el => {
+      return `<li><img width="50" height=30" src="${el.flags.svg}">${el.name.official}</li>`;
+    })
+    .join(''));
+}
+
+function createMarkupCard(array) {
+  return (markupCard = array
+    .map(({ flags, name, capital, population, languages }) => {
+      return `<img width="50" height=30" src="${flags.svg}"><h2>${
+        name.official
+      }</h2><p><b>Capital:</b> ${
+        capital[0]
+      }</p><p><b>Population:</b> ${population}<p><b>Languages:</b> ${Object.values(
+        languages
+      )}</p></p>`;
+    })
+    .join(''));
+}
+
+function renderList(markup) {
+  refs.countryInfoEl.innerHTML = '';
+  refs.countryListEl.innerHTML = markup;
+}
+
+function renderCard(markup) {
+  refs.countryListEl.innerHTML = '';
+  refs.countryInfoEl.innerHTML = markup;
+}
+
+function onError(error) {
+  console.log(error);
 }
